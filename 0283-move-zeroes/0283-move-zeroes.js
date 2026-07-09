4153e46ae0e3fef65ccee0d87ad840e9d0ef008b
Aakash Shrestha <106760169+aakashstha1@users.noreply.github.com>
2026-07-09 13:33:48 +0545

    Time: 3 ms (38.41%), Space: 60 MB (70.73%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -3,22 +3,14 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-    if(nums.length==0) return "";
-    let arr1=[]
-    let arr2=[]
-    for(let i=0;i<nums.length;i++){
-        if(nums[i]==0){
-            arr2.push(nums[i]);
-        }else{
-            arr1.push(nums[i]);
-        }
+    let s=0;
+    for(let i=0; i<nums.length;i++){
+       if(nums[i]!==0){
+            [nums[i],nums[s]]=[nums[s],nums[i]];
+            s++;
+       }
     }
-    const new_arr = [...arr1,...arr2]
-    for(let i=0;i<nums.length;i++){
-        nums[i]=new_arr[i]
-    }
-    return nums;
-    
+
 };
 
-console.log(moveZeroes([0,1,0,3,12]));
+console.log(moveZeroes([1,2,0,3,0,4]));
