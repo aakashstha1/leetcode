58c69a96612f8f66340d5b43a0e23bc12f8140da
Aakash Shrestha <106760169+aakashstha1@users.noreply.github.com>
2026-07-13 18:16:41 +0545

    Time: 22 ms (87.92%), Space: 70.6 MB (39.62%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
@@ -1,53 +1,89 @@
 /**
- * @param {number[]} nums
- * @param {number} k
- * @return {number}
+ * Find the kth largest element using Quick Select
+ * with 3-way partitioning (Dutch National Flag).
  */
-
 var findKthLargest = function(nums, k) {
-    let target=k-1;
-    let l=0
-    let r= nums.length-1;
-   
+    // Convert kth largest to target index
+    // Example: k = 4 => target index = 3
+    let target = k - 1;
 
-    while(l<=r){
-        let randomIndex = l+ Math.floor(Math.random()*(r-l+1));
-    
-        [nums[r],nums[randomIndex]]=[nums[randomIndex],nums[r]];
-    
-        let [lt,gt]=partition(nums,l,r);
+    let l = 0;
+    let r = nums.length - 1;
 
-        if(target>=lt && target <= gt){
+    while (l <= r) {
+
+        // Choose a random pivot to avoid worst-case partitions
+        let randomIndex =
+            l + Math.floor(Math.random() * (r - l + 1));
+
+        // Move pivot to the end
+        [nums[r], nums[randomIndex]] =
+            [nums[randomIndex], nums[r]];
+
+        // Partition into:
+        // > pivot | == pivot | < pivot
+        let [lt, gt] = partition(nums, l, r);
+
+        // If target lies in the equal region,
+        // we've found the answer
+        if (target >= lt && target <= gt) {
             return nums[target];
-        }else if(target<lt){
-           r=lt-1;
-        }else{
-            l=gt+1;
+        }
+
+        // Search left side (> pivot region)
+        else if (target < lt) {
+            r = lt - 1;
+        }
+
+        // Search right side (< pivot region)
+        else {
+            l = gt + 1;
         }
     }
 };
 
-function partition(nums,l,r){
-    let p = nums[r];
-    let lt = l;
-    let i=l;
-    let gt=r;
+function partition(nums, l, r) {
+    let pivot = nums[r];
 
-    while(i<=gt){
-        if(nums[i]>p){
-            [nums[i],nums[lt]]=[nums[lt],nums[i]];
-            i++;
+    // lt: start of equal region
+    let lt = l;
+
+    // current element being processed
+    let i = l;
+
+    // gt: end of equal region
+    let gt = r;
+
+    while (i <= gt) {
+
+        // Move larger elements to the left
+        if (nums[i] > pivot) {
+            [nums[i], nums[lt]] =
+                [nums[lt], nums[i]];
+
             lt++;
-        }else if(nums[i]<p){
-            [nums[i],nums[gt]]=[nums[gt],nums[i]];
+            i++;
+        }
+
+        // Move smaller elements to the right
+        else if (nums[i] < pivot) {
+            [nums[i], nums[gt]] =
+                [nums[gt], nums[i]];
+
             gt--;
-        }else{
+
+            // Don't increment i here.
+            // The swapped element needs inspection.
+        }
+
+        // Equal to pivot
+        else {
             i++;
         }
     }
-    
-    return [lt,gt];
-}
- 
 
-console.log(findKthLargest([3,2,1,5,6,4],4));
+    // Return boundaries of equal region
+    return [lt, gt];
+}
+
+console.log(findKthLargest([3,2,1,5,6,4], 4));
