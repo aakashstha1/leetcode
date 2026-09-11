a149172a99a64bdd01fd80b711ebc34f849098bb
Aakash Shrestha <106760169+aakashstha1@users.noreply.github.com>
2026-09-11 22:15:39 +0545

    Time: 0 ms (100%), Space: 55.5 MB (5.53%) - LeetHub

---
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function(nums) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left < right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (nums[mid] > nums[right]) {
+            // Minimum is on the right side
+            left = mid + 1;
+        } else {
+            // Minimum is at mid or on the left side
+            right = mid;
+        }
+    }
+
+    return nums[left];
+};
+
+const nums = [3, 4, 5, 1, 2];
+console.log(findMin(nums));
